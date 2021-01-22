1344aa2432d26fcdeb683bf1862807e08641b7d6
Teenoi <tlek7559@gmail.com>
2021-01-22 10:01:39 +0700

    remove auto update

--- lib/src/my_app.dart
+++ lib/src/my_app.dart
@@ -50,6 +50,7 @@ class MyApp extends StatelessWidget {
           home: FutureBuilder(
             future:  checkIsLogin(),
             builder: (context, snapshot) {
+
               return LoginPage();
             },
           )
